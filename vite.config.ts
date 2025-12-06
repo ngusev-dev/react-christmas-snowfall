8500cd192bea8ef1af148bc9bb9c39f3b06f9e25
TankistPro <nakol.nayk@mail.ru>
2025-12-06 21:41:47 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import dts from 'vite-plugin-dts'
-// import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    // dts({ 
-    //   include: ['lib'],
-    //    tsconfigPath: './tsconfig-build.json'
-    // })
+    dts({ 
+      include: ['lib'],
+       tsconfigPath: './tsconfig-build.json'
+    })
   ],
-  // build: {
-  //   copyPublicDir: false,
-  //   lib: {
-  //     entry: resolve(__dirname, 'lib/index.ts'),
-  //     formats: ['es'],
-  //   },
-  //   rollupOptions: {
-  //     external: ['react', 'react/jsx-runtime'],
-  //     output: {
-  //      assetFileNames: 'assets/[name][extname]',
-  //      entryFileNames: '[name].js',
-  //    }
-  //   }
-  // },
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      output: {
+       assetFileNames: 'assets/[name][extname]',
+       entryFileNames: '[name].js',
+     }
+    }
+  },
 })
