9e61ebcea020371434c0e66ffe1e6d8446566888
TankistPro <nakol.nayk@mail.ru>
2025-12-10 23:20:28 +0300

    feat: added control components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
+import { useState } from 'react';
 import { ChristmasSnowfall } from '../lib/components/Snowfall';
 import styles from './App.module.css';
 
+import { Slider } from '@base-ui-components/react/slider';
+import { Switch } from '@base-ui-components/react';
+import { APPEARANCE_TYPE } from '../lib/types/snowfall.types';
+
 function App() {
+  const [snowflakeCount, setSnowflakeCount] = useState(100)
+  const [appearanceType, setAppearanceType] = useState<keyof typeof APPEARANCE_TYPE>(APPEARANCE_TYPE.CIRCLE)
   return (
     <div className={styles.page}>
-      <ChristmasSnowfall appearance='CIRCLE' />
+      <ChristmasSnowfall snowflakeCount={snowflakeCount} appearance={appearanceType}/>
+
+      <div className={styles.modal}>
+        <Slider.Root defaultValue={snowflakeCount} min={0} max={500} onValueChange={(value) => setSnowflakeCount(value)}>
+          <Slider.Value />
+          <Slider.Control className={styles.Control}>
+            <Slider.Track className={styles.Track}>
+              <Slider.Indicator className={styles.Indicator} />
+              <Slider.Thumb className={styles.Thumb} />
+            </Slider.Track>
+          </Slider.Control>
+        </Slider.Root>
+
+
+      <div className={styles.SwitchStyle}>
+        <p style={{
+          opacity: appearanceType === APPEARANCE_TYPE.CIRCLE ? 1 : 0.4
+        }}>Circle</p>
+        <Switch.Root 
+          checked={appearanceType === APPEARANCE_TYPE.SNOWFLAKE} 
+          className={styles.Switch} 
+          onCheckedChange={(checked) => checked ? setAppearanceType(APPEARANCE_TYPE.SNOWFLAKE) : setAppearanceType(APPEARANCE_TYPE.CIRCLE)}
+        >
+          <Switch.Thumb className={styles.SwitchThumb} />
+        </Switch.Root>
+        <p style={{
+          opacity: appearanceType === APPEARANCE_TYPE.SNOWFLAKE ? 1 : 0.4
+        }}>Snowflake</p>
+      </div>
+
+      </div>
+      
     </div>
   )
 }
